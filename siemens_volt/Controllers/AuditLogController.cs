a0e63399b980437f773294308790e4b45c4c6c1c
Miroslav Bartos <mirek-b@seznam.cz>
2020-08-03 18:55:18 +0200

    Create view and logic for Audit Log

---
+++ siemens_volt/Controllers/AuditLogController.cs
@@ -0,0 +1,153 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using siemens_volt.Data;
+using siemens_volt.Models;
+
+namespace siemens_volt
+{
+    public class AuditLogController : Controller
+    {
+        private readonly ApplicationDbContext _context;
+
+        public AuditLogController(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        // GET: AuditLog
+        public async Task<IActionResult> Index()
+        {
+            return View(await _context.AuditLog.ToListAsync());
+        }
+
+        // GET: AuditLog/Details/5
+        public async Task<IActionResult> Details(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var auditLog = await _context.AuditLog
+                .FirstOrDefaultAsync(m => m.ID == id);
+            if (auditLog == null)
+            {
+                return NotFound();
+            }
+
+            return View(auditLog);
+        }
+
+        // GET: AuditLog/Create
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: AuditLog/Create
+        // To protect from overposting attacks, enable the specific properties you want to bind to, for 
+        // more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create([Bind("Timestamp,Action,Description")] AuditLog auditLog)
+        {
+            if (ModelState.IsValid)
+            {
+                _context.Add(auditLog);
+                await _context.SaveChangesAsync();
+                return RedirectToAction(nameof(Index));
+            }
+            return View(auditLog);
+        }
+
+        // GET: AuditLog/Edit/5
+        public async Task<IActionResult> Edit(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var auditLog = await _context.AuditLog.FindAsync(id);
+            if (auditLog == null)
+            {
+                return NotFound();
+            }
+            return View(auditLog);
+        }
+
+        // POST: AuditLog/Edit/5
+        // To protect from overposting attacks, enable the specific properties you want to bind to, for 
+        // more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(int id, [Bind("ID,Timestamp,Action,Description")] AuditLog auditLog)
+        {
+            if (id != auditLog.ID)
+            {
+                return NotFound();
+            }
+
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    _context.Update(auditLog);
+                    await _context.SaveChangesAsync();
+                }
+                catch (DbUpdateConcurrencyException)
+                {
+                    if (!AuditLogExists(auditLog.ID))
+                    {
+                        return NotFound();
+                    }
+                    else
+                    {
+                        throw;
+                    }
+                }
+                return RedirectToAction(nameof(Index));
+            }
+            return View(auditLog);
+        }
+
+        // GET: AuditLog/Delete/5
+        public async Task<IActionResult> Delete(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var auditLog = await _context.AuditLog
+                .FirstOrDefaultAsync(m => m.ID == id);
+            if (auditLog == null)
+            {
+                return NotFound();
+            }
+
+            return View(auditLog);
+        }
+
+        // POST: AuditLog/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> DeleteConfirmed(int id)
+        {
+            var auditLog = await _context.AuditLog.FindAsync(id);
+            _context.AuditLog.Remove(auditLog);
+            await _context.SaveChangesAsync();
+            return RedirectToAction(nameof(Index));
+        }
+
+        private bool AuditLogExists(int id)
+        {
+            return _context.AuditLog.Any(e => e.ID == id);
+        }
+    }
+}
