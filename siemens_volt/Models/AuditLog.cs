574142c67c3e383f28e7d04a3e301449d273d0f9
Miroslav Bartos <mirek-b@seznam.cz>
2020-08-03 19:38:14 +0200

    Format timestamp

--- siemens_volt/Models/AuditLog.cs
+++ siemens_volt/Models/AuditLog.cs
@@ -12,8 +12,17 @@ namespace siemens_volt.Models
         [Key]
         [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
         public int ID { get; set; }
+
+        [DisplayFormat(DataFormatString = "{0:dd.MM.yyyy hh:mm}")]
         public DateTime Timestamp { get; set; }
         public string Action { get; set; }
         public string Description { get; set; }
+
+        public AuditLog(DateTime timestamp, string action, string description)
+        {
+            Timestamp = timestamp;
+            Action = action;
+            Description = description;
+        }
     }
 }
