051ddd5b81f223c7ddfec5f80538c54c5f9b1082
Miroslav Bartos <mirek-b@seznam.cz>
2020-08-03 18:27:02 +0200

    Create model class for audit log

---
+++ siemens_volt/Models/AuditLog.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace siemens_volt.Models
+{
+    public class AuditLog
+    {
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public int ID { get; set; }
+        public DateTime Timestamp { get; set; }
+        public string Action { get; set; }
+        public string Description { get; set; }
+    }
+}
