4da23f13f62aa37a5604a3ded67bf19f8f02060a
Miroslav Bartos <mirek-b@seznam.cz>
2020-08-03 17:59:56 +0200

    Customize register view and logic

---
+++ siemens_volt/Areas/Identity/IdentityHostingStartup.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.UI;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using siemens_volt.Data;
+
+[assembly: HostingStartup(typeof(siemens_volt.Areas.Identity.IdentityHostingStartup))]
+namespace siemens_volt.Areas.Identity
+{
+    public class IdentityHostingStartup : IHostingStartup
+    {
+        public void Configure(IWebHostBuilder builder)
+        {
+            builder.ConfigureServices((context, services) => {
+            });
+        }
+    }
+}
